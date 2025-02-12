f2ea3c4e0d3b8bf3ff4f7d4a7a0bced911adcd1a
LAB13_USER ON WS31 <L13X13W31@citulab.net>
2025-02-12 13:20:04 +0800

    add backend files

---
+++ backend/src/test/java/edu/cit/petshopsystem/PetshopsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.cit.petshopsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetshopsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
