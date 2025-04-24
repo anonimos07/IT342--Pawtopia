342321c7b772026e9172c29a9c854be9dd76e729
Jared12388 <canasajared666@gmail.com>
2025-04-24 19:12:21 +0800

    cleared mobile

--- frontend_mobile/app/src/main/java/com/example/pawtopia/model/UserProfile.kt
+++
@@ -1,9 +0,0 @@
-package com.example.pawtopia.model
-
-data class UserProfile(
-    val username: String,
-    val firstName: String,
-    val lastName: String,
-    val email: String,
-    val role: String
-)
